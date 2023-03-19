a9a6ec6410a83fd3aae8a32d17357344f07a0e43
chibuike umechukwu <umebuike@gmail.com>
2023-03-19 19:56:19 +0100

    update

--- src/routes/document.route.js
+++ src/routes/document.route.js
@@ -3,7 +3,9 @@ const documentController = require('../controllers/document.controller')
 
 const router = Router()
 
-router.route('/').get(documentController.getAll).post(documentController.create)
+router.route('/')
+	// .get(documentController.getAll)
+	.post(documentController.create)
 router.route('/idempotency-key').get(documentController.getIdempotencyKey)
 
 router
