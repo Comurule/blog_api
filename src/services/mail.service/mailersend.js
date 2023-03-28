8fa0fae924bf6f723e2453c9c88aba037d6e9c35
chibuike umechukwu <umebuike@gmail.com>
2023-03-28 12:13:43 +0100

    ...

--- src/services/mail.service/mailersend.js
+++ src/services/mail.service/mailersend.js
@@ -18,34 +18,29 @@ module.exports = async (template, isBulk = false) => {
 
   const sentFrom = new Sender(template.sender.email, template.sender.name);
 
-  const recipients = template.recipients.map(x => new Recipient(x.email));
-  const emailParams = new EmailParams()
-    .setFrom(sentFrom)
-    .setReplyTo(sentFrom)
-    .setPersonalization(template.recipients)
-    .setSubject(template.subject)
-    .setTemplateId(template.templateId);
-
   if (isBulk) {
-    emailParams
-      .setTo([new Recipient('val.umechukwu@gmail.com')])
-      .setBcc(recipients)
+    const recipients = template.recipients.map(x => new EmailParams()
+      .setFrom(sentFrom)
+      .setReplyTo(sentFrom)
+      .setTo([new Recipient(x.email)])
+      .setPersonalization([x])
+      .setSubject(template.subject)
+      .setTemplateId(template.templateId)
+    );
+
+    await mailerSend.email.sendBulk(recipients);
   } else {
-    emailParams.setTo(recipients)
+    const recipients = template.recipients.map(x => new Recipient(x.email));
+    const emailParams = new EmailParams()
+      .setFrom(sentFrom)
+      .setReplyTo(sentFrom)
+      .setTo(recipients)
+      .setPersonalization(template.recipients)
+      .setSubject(template.subject)
+      .setTemplateId(template.templateId);
+
+    await mailerSend.email.send(emailParams);
   }
 
-  await mailerSend.email.send(emailParams);
 
-
-  // const recipients = template.recipients.map(x => new EmailParams()
-  //   .setFrom(sentFrom)
-  //   .setTo([new Recipient(x.email)])
-  //   .setReplyTo(sentFrom)
-  //   .setPersonalization(x)
-  //   .setSubject(template.subject)
-  //   .setTemplateId(template.templateId)
-  //   .setPrecedenceBulk(true)
-  // );
-
-  // await mailerSend.email.sendBulk(recipients);
 }
